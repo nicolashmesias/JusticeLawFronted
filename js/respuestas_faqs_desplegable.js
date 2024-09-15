e3c0c79e5fbcd1357001636f5cff618b85ae1826
Rich20067 <ruizricardo504@gmail.com>
2024-09-15 14:58:19 -0500

    .

--- js/respuestas_faqs_desplegable.js
+++ js/respuestas_faqs_desplegable.js
@@ -1,23 +1,22 @@
 document.querySelectorAll('.faq-item').forEach(function(faqItem) {
     faqItem.addEventListener('click', function() {
-        const answer = this.nextElementSibling; // Selecciona el siguiente elemento, que es la respuesta
-        const icon = this.querySelector('.plus-icon'); // Selecciona el ícono dentro de la pregunta
+        const answer = this.nextElementSibling; 
+        const icon = this.querySelector('.plus-icon'); 
         
-        // Alternar la visibilidad de la respuesta
         if (answer.style.display === 'block' || answer.style.display === '') {
-            answer.style.display = 'none';  // Si está visible, ocúltala
-            icon.src = "../../img/mas.png";  // Cambia el ícono a "+"
+            answer.style.display = 'none';  
+            icon.src = "../../img/mas.png";  
         } else {
-            // Cerrar cualquier otro desplegable que esté abierto
+            
             document.querySelectorAll('.faq-answer').forEach(function(ans) {
-                ans.style.display = 'none';  // Ocultar todas las respuestas
+                ans.style.display = 'none';  
             });
             document.querySelectorAll('.plus-icon').forEach(function(icon) {
-                icon.src = "../../img/mas.png";  // Cambiar todos los íconos a "+"
+                icon.src = "../../img/mas.png";  
             });
             
-            answer.style.display = 'block'; // Si está oculta, muéstrala
-            icon.src = "../../img/cerrarsecion.png";  // Cambia el ícono a "-"
+            answer.style.display = 'block'; 
+            icon.src = "../../img/cerrarsecion.png"; 
         }
     });
 });
